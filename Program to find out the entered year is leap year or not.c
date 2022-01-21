c7942fe40130dd03f65c6ed65831b8934e767f97
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-01-21 08:30:20 +0530

    #include<stdio.h> int main() {     int y,a,b;     scanf("%d",&y);     if(y%4==0)     {         if(y%100==0)         {             if(y%400==0)             {                 printf("True");             }             else             {                 printf("False");             }         }         else         {             printf("True");         }     }     else     {         printf("False");     } }

---
+++ Program to find out the entered year is leap year or not.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int y,a,b;
+    scanf("%d",&y);
+    if(y%4==0)
+    {
+        if(y%100==0)
+        {
+            if(y%400==0)
+            {
+                printf("True");
+            }
+            else
+            {
+                printf("False");
+            }
+        }
+        else
+        {
+            printf("True");
+        }
+    }
+    else
+    {
+        printf("False");
+    }
+}
