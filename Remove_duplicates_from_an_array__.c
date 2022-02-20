3c6f405a9e6a7a450a45624b75171591ef0f88c8
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-02-20 21:19:48 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,j;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(arr[i]==arr[j] && i!=j)             {                 arr[j]=1000;             }         }         if(arr[i]!=1000)         {             printf("%d ",arr[i]);         }     } }

---
+++ Remove_duplicates_from_an_array__.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,j;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j] && i!=j)
+            {
+                arr[j]=1000;
+            }
+        }
+        if(arr[i]!=1000)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+}
