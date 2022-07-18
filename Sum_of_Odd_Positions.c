97d79a50711c1a8d0f2e2f8834851d8a1dbeb4c9
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-07-18 22:23:28 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,s1=0,s2=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(i%2!=0)             s1=s1+arr[i];
        }
        
            printf("%d",s1);
         
    }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,s1=0,s2=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(i%2!=0)
+            s1=s1+arr[i];
+        
+    }
+    
+        printf("%d",s1);
+     
+}
