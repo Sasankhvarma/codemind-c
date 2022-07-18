306211028094623e82c98cd8724409442bf90a47
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-07-18 22:41:54 +0530

    #include<stdio.h> int main() {     int n,s=0;     scanf("%d",&n);     int arr[n],i,s1=0,s2=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int k=arr[0];     for(i=0;i<n;i++)     {         if(arr[i]>k)         {             k=arr[i];         }     }     printf("%d",k); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0;
+    scanf("%d",&n);
+    int arr[n],i,s1=0,s2=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int k=arr[0];
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>k)
+        {
+            k=arr[i];
+        }
+    }
+    printf("%d",k);
+}
