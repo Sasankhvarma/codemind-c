0a437516817b5572ebe77a73d89b55d6dc7b4b73
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-07-18 21:55:27 +0530

    #include<stdio.h> #include<math.h> int dig(int n) {     int q,r,c=0;     q=n;     while(q!=0)     {         c++;         q/=10;     }     return c; } int main() {     int n,c,s=0,q,r;     scanf("%d",&n);     c=dig(n);     q=n;     while(q!=0)     {        r=q%10;        s=s+pow(r,c);        c--;        q=q/10;     }     if(s==n)     {         printf("True");     }     else     {         printf("False");     } }

--- Disarium_number.c
+++ Disarium_number.c
@@ -1,24 +1,30 @@
 #include<stdio.h>
 #include<math.h>
+int dig(int n)
+{
+    int q,r,c=0;
+    q=n;
+    while(q!=0)
+    {
+        c++;
+        q/=10;
+    }
+    return c;
+}
 int main()
 {
-    int n,sq,c=0,sum=0,temp,r,rev=0;
+    int n,c,s=0,q,r;
     scanf("%d",&n);
-    temp=n;
-    while(temp>0)
+    c=dig(n);
+    q=n;
+    while(q!=0)
     {
-        r=temp%10;
-        rev=(rev*10)+r;
-        temp=temp/10;
+       r=q%10;
+       s=s+pow(r,c);
+       c--;
+       q=q/10;
     }
-    for(int i=1;rev>0;i++)
-    {
-        r=rev%10;
-        sq=pow(r,i);
-        sum = sum+sq;
-        rev=rev/10;
-    }
-    if(sum==n)
+    if(s==n)
     {
         printf("True");
     }
